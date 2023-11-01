fe65a769628289bb7810b97e3b08dfe4cc9bef41
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2023-10-31 21:29:09 -0600

    Integrantes

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+# Integrantes:
+# Juergen Pérez Céspedes
+# Hillary Cruz Valenzuela
+# Andrés Vega Hidalgo
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    print("Hola mundo")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
