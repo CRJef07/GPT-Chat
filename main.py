1e9900dc105070740c6dd3cfea0accc7e4c8010b
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2023-11-20 13:44:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,24 @@
 # Hillary Cruz Valenzuela
 # Andrés Vega Hidalgo
 
-import numpy as np
-import pandas as pd
-import re
-import nltk
-import matplotlib.pyplot as plt
+from openai import OpenAI
+import os
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
 
 def main():
-    print("Hola mundo")
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
+    )
+
+    print(completion.choices[0].message)
 
 
 if __name__ == '__main__':
