f6685020f6efaec974b6ae6b84bc3fa9afa5cbdb
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2023-11-20 16:44:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,80 +7,64 @@ from openai import OpenAI
 import gradio as gr
 import os
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+class AnimalChatbot:
+    def __init__(self):
+        self.OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+        self.client = OpenAI(api_key=self.OPENAI_API_KEY)
+        self.messages = [
+            {"role": "system", "content": "Este chat gpt solo responde preguntas referentes a animales. Para preguntas no "
+                                          "relacionadas a animales, el bot responde con No entiendo"}
+        ]
 
-messages = [
-    {"role": "system", "content": "Este chat gpt solo responde preguntas referentes a animales. Para preguntas no "
-                                  "relacionadas a animales, el bot responde con No entiendo"},
+    def is_animal_related(self, input_text):
+        animal_keywords = ["animal", "mamífero", "ave", "reptil", "anfibio", "pez", "invertebrado", "hola"]
+        return any(keyword in input_text.lower() for keyword in animal_keywords)
 
-    {"role": "user", "content": "¿Qué es un animal?"},
+    def generate_response(self, prompt):
+        if prompt:
+            if self.is_animal_related(prompt):
+                self.messages.append({"role": "user", "content": prompt})
+                chat = self.client.chat.completions.create(
+                    model="gpt-3.5-turbo-1106", messages=self.messages
+                )
+                reply = chat.choices[0].message.content
+                self.messages.append({"role": "assistant", "content": reply})
+            else:
+                reply = "No entiendo. Por favor, hazme preguntas relacionadas con animales."
 
-    {"role": "assistant", "content": "Un animal es un ser vivo perteneciente al reino animalia, que constituye un "
-                                     "grupo biológico que incluye una amplia variedad de organismos. Los animales son "
-                                     "organismos multicelulares, lo que significa que están formados por más de una "
-                                     "célula, y son heterótrofos, lo que implica que obtienen su alimento consumiendo "
-                                     "otros organismos o sustancias orgánicas. Los animales presentan una diversidad "
-                                     "increíble en términos de forma, tamaño, comportamiento y hábitat. Pueden "
-                                     "clasificarse en diferentes grupos, como mamíferos, aves, reptiles, anfibios, "
-                                     "peces e invertebrados. Cada grupo tiene características únicas que los "
-                                     "distinguen. Además, los animales desempeñan roles importantes en los "
-                                     "ecosistemas, ya que participan en cadenas alimentarias, contribuyen a la "
-                                     "polinización de las plantas, ayudan en la descomposición de materia orgánica y "
-                                     "desempeñan numerosas funciones en los diversos hábitats de la Tierra."}
-]
+        return reply
 
-
-def is_animal_related(input_text):
-    animal_keywords = ["animal", "mamífero", "ave", "reptil", "anfibio", "pez", "invertebrado", "hola"]
-    return any(keyword in input_text.lower() for keyword in animal_keywords)
-
-
-def generate_response(prompt):
-    if prompt:
-        if is_animal_related(prompt):
-            messages.append({"role": "user", "content": prompt})
-            chat = client.chat.completions.create(
-                model="gpt-3.5-turbo-1106", messages=messages
-            )
-            reply = chat.choices[0].message.content
-            messages.append({"role": "assistant", "content": reply})
-        else:
-            reply = "No entiendo. Por favor, hazme preguntas relacionadas con animales."
-
-    return reply
+    def chat(self, input, history):
+        history = history or []
+        my_history = list(sum(history, ()))
+        my_history.append(input)
+        my_input = ' '.join(my_history)
+        output = self.generate_response(my_input)
+        history.append((input, output))
+        return history, history
 
 
-def my_chatbot(input, history):
-    history = history or []
-    my_history = list(sum(history, ()))
-    my_history.append(input)
-    my_input = ' '.join(my_history)
-    output = generate_response(my_input)
-    history.append((input, output))
-    return history, history
+def main():
+    animal_chatbot = AnimalChatbot()
 
-
-with gr.Blocks() as bot:
-    gr.Markdown("""
-            <div style="background-color: #ffcccb; padding: 20px;">
-                <h1 style="color: #008000; font-family: Arial; text-align: center;">
+    with gr.Blocks() as bot:
+        gr.Markdown("""
+            <div style="background-color: #41465f; padding: 20px; border-radius:10px;">
+                <h1 style="color: #ffffffff; font-family: Arial; text-align: center;">
                     Chat sobre Animales
                 </h1>
             </div>
         """)
-    chatbot = gr.Chatbot()
-    chatbot.label = "Chat sobre animales"
-    state = gr.State()
-    text = gr.Textbox(placeholder="Hola, pregúntame algo animales. -> DALE ENTER PARA ENVIAR PREGUNTA", label="Pregunta -> DALE ENTER PARA ENVIAR PREGUNTA")
-    text.submit(fn=my_chatbot, inputs=[text, state], outputs=[chatbot, state])
+        chatbot = gr.Chatbot()
+        chatbot.label = "Chat sobre animales"
+        state = gr.State()
+        text = gr.Textbox(placeholder="Hola, pregúntame algo animales. -> DALE ENTER PARA ENVIAR PREGUNTA", label="Pregunta -> DALE ENTER PARA ENVIAR PREGUNTA")
+        text.submit(fn=animal_chatbot.chat, inputs=[text, state], outputs=[chatbot, state])
 
-    submit = gr.Button("ENVIAR RESPUESTA")
-    submit.click(my_chatbot, inputs=[text, state], outputs=[chatbot, state])
+        submit = gr.Button("ENVIAR RESPUESTA")
+        submit.click(animal_chatbot.chat, inputs=[text, state], outputs=[chatbot, state])
 
-
-def main():
     bot.launch(share=True)
 
 
